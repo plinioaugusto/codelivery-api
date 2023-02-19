b6479d1d6ed146e5f408463c25b6a45e84cf4acc
Plínio Augusto <plinioaugustoadm@hotmail.com>
2023-02-19 19:16:47 -0300

    feat: :sparkles: add config envs database in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [RoutesModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    ConfigModule.forRoot(),
+    RoutesModule,
+    MongooseModule.forRoot(process.env.MONGO_DSN, { useNewUrlParser: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
